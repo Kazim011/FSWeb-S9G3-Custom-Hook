0fc1a02caef2c1a01a62919770ab329b5bde2b40
kazım <kazimnergiz123@gmail.com>
2023-01-25 23:05:26 +0300

    S9G3

---
+++ frontend/components/hooks/GeceModu.js
@@ -0,0 +1,16 @@
+import { useState } from "react";
+import localStorageKullan from "./localStorageKullan";
+
+const { setItem, getItem } = localStorageKullan();
+
+export default function GeceModu() {
+  const [geceModu, setGeceModu] = useState(getItem("geceModu"));
+
+  const toggleMode = () => {
+    setItem("geceModu", !geceModu);
+    //setGeceModu(!geceModu);
+    setGeceModu(getItem("geceModu"));
+  };
+
+  return { geceModu, toggleMode };
+}
